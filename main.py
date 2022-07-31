051e5e66be346892789757f7feb90c1c67e0aeb3
Sambhu_suresh <50201955+sambhusuresh@users.noreply.github.com>
2022-07-31 19:29:29 +0530

    huh

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import random
 
-r = random.randrange(11)
-print(r)
+import random
+top = input('type a number ')
+if top.isdigit():
+    top = int(top)
+    if top < 0:
+        print("please enter a number more than zero ")
+    else:
+        quit()
+else:
+    print("plese enter a number")
+    quit()
+
+r_number = random.randrange(11)
+print(r_number)
+
+
+ 
